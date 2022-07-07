a8181f05c2cd221264616a8cd7d36b09aac09231
Dinken <lurdan4@gmail.com>
2022-07-07 22:18:58 +0200

    Delete main.dart

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:html';
-
-import 'package:flutter/material.dart';
-
-void main() {
-  String msg = "Hello";
-  print(ObjectElement.created().contentWindow.postMessage(message, targetOrigin).bottomCenter)
-}
